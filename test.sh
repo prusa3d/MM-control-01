19b2c42f9efae39ed4552a361bfa3b0bed0f0cf4
Marek Bel <marek.bel@posta.cz>
2018-11-12 18:54:16 +0100

    Generate build scripts of unit tests to separate directory.

--- test.sh
+++ test.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 cd ..
-if [ ! -d "MM-control-01-build" ]; then
-    mkdir MM-control-01-build || exit 1
+if [ ! -d "MM-control-01-test" ]; then
+    mkdir MM-control-01-test || exit 1
 fi
 
-cd MM-control-01-build || exit 2
+cd MM-control-01-test || exit 2
 rm -r *
 cmake -G "Eclipse CDT4 - Ninja" ../MM-control-01/Tests || exit 3
 cmake --build . || exit 4
