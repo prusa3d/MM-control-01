f1f4765f4d9d668f7705f3d9c0738f71e10e9559
Marek Bel <marek.bel@posta.cz>
2019-04-24 19:04:05 +0200

    Disable pulley current earlier than disengage idler, as this is faster operation.

--- MM-control-01/mmctl.cpp
+++ MM-control-01/mmctl.cpp
@@ -408,8 +408,9 @@ void load_filament_withSensor()
     }
 
     motion_feed_to_bondtech();
-    motion_disengage_idler();
+
     tmc2130_disable_axis(AX_PUL, tmc2130_mode);
+    motion_disengage_idler();
     isFilamentLoaded = true;  // filament loaded
 }
 
